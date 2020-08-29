8560b6f282d32a58d4bd2fbf160ce8afd67f9e90
carlinhosmugiwara <55931229+carlinhosmugiwara@users.noreply.github.com>
2020-08-28 22:48:22 -0300

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,30 +1,30 @@
-# scraping do bot de twitter
+# scraping do bot de twitter/scraping for the twitter bot
 
 import os
 import requests as rqts
 from bs4 import BeautifulSoup as btsp
 
-# site com as imagens ques serão usadas
+# site com as imagens ques serão usadas/site that has the images that will be used
 url = 'siteexemplo.com.br/imagens'
 
-#fazer o parsing da página
+#fazer o parsing da página/parsing of the page
 pagina = rqts.get('url')
 soup = btsp(pagina.text, 'html.parser')
 
-# agora localizar todos os elementos com a tag de imagem
+# agora localizar todos os elementos com a tag de imagem/find all image tag elements
 img_tag = soup.findAll('img')
 
-# criar diretório com imagens do site
+# criar diretório com imagens do site/create directory with these images
 if not os.path.exists('nome_dir'):
   os.makedirs('nome_dir')
 
-# entrar no diretório criado
+# entrar no diretório criado/navegate through the directory
 os.chdir('nome_dir')
 
-# variável para nomear de forma genérica as imagens
+# variável para nomear de forma genérica as imagens/variable to name the images in a generic way
 va = 0
 
-# colocando imagens no diretório
+# colocando imagens no diretório/putting images in the directory
 for img in img_tag:
   try:
     url = img[src']
