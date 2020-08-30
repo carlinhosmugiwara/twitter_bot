5812c032d88cb3a7f4ba6a5bfb6c0f43f8aca5f7
carlinhosmugiwara <55931229+carlinhosmugiwara@users.noreply.github.com>
2020-08-29 23:52:04 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,26 @@
-# bot de twitter feito em python para postar imagens/twitter bot coded in python to post images  
-# para que seu código rode, você deve primeiro criar uma conta developer no twitter/you need a developer account for this to work
+# twitter bot coded in python to post images  
+# you need a developer account for this to work
 
 import os
 import time
 import tweepy as tp
 
-# dados importantes de sua conta developer/important data from your account
-key_conta = "Coloque a chave de sua conta aqui"
-secret_conta = "Coloque o secret de sua conta aqui"
+# important data from your account
+account_key = "Put your account key here"
+account_secret = "Put your account secret here"
 
-token_acesso = "Insira o token de acesso aqui"
-secret_acesso = "Insira o secret de acesso aqui"
+access_token = "Put your access token here"
+access_secret = "Put your account secret here"
 
-#autenticar informações e usar api/authenticate informations to use the api
-autenticacao = tp.OAuthHandler(key_conta, secret_conta)
-autenticacao.set_access_token(token_acesso, secret_acesso)
-api = tp.API(autenticacao)
+# authenticate informations to use the api
+authentication = tp.OAuthHandler(account_key, account_secret)
+authentication.set_access_token(access_token, access_secret)
+api = tp.API(authentication)
 
-#iterar imagens do diretório criada no scraping.py/iterate images from the directory created in scraping.py
-os.chdir('nome_dir')
-for imagem in os.listdir('.'):
-  api.update_with_media(imagem)
+# iterate images from the directory created in scraping.py
+os.chdir('directory_name')
+for image in os.listdir('.'):
+  api.update_with_media(image)
   time.sleep(100)
 
 
