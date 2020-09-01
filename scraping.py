a39327f1f3b9762ada4dd16a12426115612bce51
carlinhosmugiwara <55931229+carlinhosmugiwara@users.noreply.github.com>
2020-09-01 01:50:17 -0300

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,38 +1,37 @@
-# scraping do bot de twitter/scraping for the twitter bot
+# scraping for the twitter bot
 
 import os
 import requests as rqts
-from bs4 import BeautifulSoup as btsp
+from bs4 import BeautifulSoup as btsp # library to do the actual scraping
 
-# site com as imagens ques serão usadas/site that has the images that will be used
-url = 'siteexemplo.com.br/imagens'
+# site that has the images that will be used
+url = 'examplesite.com/images'
 
-#fazer o parsing da página/parsing of the page
-pagina = rqts.get('url')
-soup = btsp(pagina.text, 'html.parser')
+# parsing of the page
+page = rqts.get('url')
+soup = btsp(page.text, 'html.parser')
 
-# agora localizar todos os elementos com a tag de imagem/find all image tag elements
+# find all image tag elements
 img_tag = soup.findAll('img')
 
-# criar diretório com imagens do site/create directory with these images
-if not os.path.exists('nome_dir'):
-  os.makedirs('nome_dir')
+# create directory with these images
+if not os.path.exists('name_dir'):
+  os.makedirs('name_dir')
 
-# entrar no diretório criado/navegate through the directory
-os.chdir('nome_dir')
+# navegate through the directory
+os.chdir('name_dir')
 
-# variável para nomear de forma genérica as imagens/variable to name the images in a generic way
+# variable to name the images in a generic way
 va = 0
 
-# colocando imagens no diretório/putting images in the directory
+# putting images in the directory
 for img in img_tag:
   try:
     url = img[src']
-    resposta = rqts.get(url)
-    if resposta.status_code == 200:
-      with(open('imagem_' + str(va) + '.jpg', 'wb') as f:
+    response = rqts.get(url)
+    if response.status_code == 200:
+      with(open('image_' + str(va) + '.png', 'wb') as f:
         f.write(rqts.get(url).content)
-        f.close()
         va+=1
-   except:
+  except:
     pass
